26caa31e3d5f2ca430050e06e85de1cdc4fe440d
medv <mikhail.advent@gmail.com>
2014-05-23 01:19:15 +1200

    added connect, first proof of concept of a touchable and clickable slider input

--- js/main.js
+++ js/main.js
@@ -1,9 +1,47 @@
 ;(function($, window, document, undefined) {
 
-	$(document).ready(function(){
+	$(document).ready(function() {
 
-		
-		
+		var viewportEl = $('#viewport'),
+			sliderEl =$('#slider'),
+			placeSlider = function (e) {
+				var pageX = e.originalEvent.x || e.originalEvent.touches[0].pageX,
+					cssX = 0;
+
+				if (pageX < 50) {
+					cssX = 50;
+				}
+				else if (pageX > (viewportEl.outerWidth() - 50)) {
+					cssX = viewportEl.outerWidth() - 50;
+				}
+				else {
+					cssX = pageX;
+				}
+
+				sliderEl.css({
+					left: cssX
+				});
+			};
+
+
+		viewportEl.on('touchstart mousedown', function(e){
+			placeSlider(e);
+
+			viewportEl.on('touchmove mousemove', function(e) {
+
+				e.preventDefault();
+				e.stopPropagation();
+
+				placeSlider(e);				
+			});
+
+			viewportEl.on('touchend mouseup', function() {
+				viewportEl.off('touchmove mousemove');
+				viewportEl.off('touchend mouseup');
+			});
+
+		});
+
 	});
 
 })(jQuery, this, document);
