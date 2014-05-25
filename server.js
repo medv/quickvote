2a27f4782b781135bedc56be8003836c49d9d2a9
medv <mikhail@oceanbrowser.com>
2014-05-26 00:32:31 +1200

    stripped out some node express stuff that was leftover

--- server.js
+++ server.js
@@ -4,22 +4,8 @@ var fs = require('fs'),
 	bodyParser = require('body-parser'),
 	app = express();
 
-var comments = [{author: 'misha', message: 'chat room activate'}];
-
 app.use('/', express.static(__dirname));
 app.use(bodyParser());
 
-app.get('/data', function(req, res) {
-	res.setHeader('Content-Type', 'application/json');
-	res.send(JSON.stringify(comments));
-});
-
-app.post('/data', function(req, res) {
-	comments.push(req.body);
-	res.setHeader('Content-Type', 'application/json');
-	res.send(JSON.stringify(comments));
-});
-
 app.listen(8080);
-
 console.log('listening to port 8080');
