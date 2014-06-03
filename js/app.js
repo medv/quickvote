cf61bc1b1360fbe041c102c54721ad33e0945b31
medv <mikhail.advent@gmail.com>
2014-06-03 12:20:39 +1200

    temporary answer input hooked up to global app state

--- js/app.js
+++ js/app.js
@@ -27,18 +27,30 @@
 			});
 		},
 
+		onSubtract: function(slide) {
+			var newState = $.extend({}, this.state);
+
+			newState.questions[slide.props.key].answer = newState.questions[slide.props.key].answer - 1;
+
+			this.setState(newState);
+		},
+
+		onAdd: function(slide) {
+			var newState = $.extend({}, this.state);
+
+			newState.questions[slide.props.key].answer = newState.questions[slide.props.key].answer + 1;
+
+			this.setState(newState);						
+		},
+
 		getInitialState: function() {
 			return {
-				pages: [
-					{title: 'Privacy', text: 'This is aaaaaaalll about privacy'},
-					{title: 'About', text: 'Some about text'}
-				],
 				questions: [
-					'Q1. Cake or Death?',
-					'Q2. Sorry, we\'re fresh out of that, could I interest you in some Death?',
-					'Q3. Just some other question...'
+					{question: 'Q1. Cake or Death?', answer: 0},
+					{question: 'Q2. Sorry, we\'re fresh out of that, could I interest you in some Death?', answer: 0},
+					{question: 'Q3. Just some other question...', answer: 0}				
 				],
-				currentSlide: 2,
+				currentSlide: 0,
 				style: {
 					width: 0,
 					height: 0
@@ -56,7 +68,15 @@
 		render: function() {
 			return (
 				<div className="App" style={this.state.style}>
-					<Slides style={this.state.style} pages={this.state.pages} questions={this.state.questions} currentSlide={this.state.currentSlide} previousSlide={this.previousSlide} nextSlide={this.nextSlide} />
+					<Slides
+						style={this.state.style}
+						questions={this.state.questions}
+						currentSlide={this.state.currentSlide}
+						previousSlide={this.previousSlide}
+						nextSlide={this.nextSlide}
+						onAdd={this.onAdd}
+						onSubtract={this.onSubtract}>
+					</Slides>
 				</div>
 			);
 		}
@@ -71,21 +91,26 @@
 
 			var style = {
 				left: -(me.props.currentSlide * me.props.style.width),
-				width: me.props.style.width * (me.props.pages.length + me.props.questions.length),
+				width: me.props.style.width * me.props.questions.length,
 				height: me.props.style.height
 			}
 
-			var pagesNodes = me.props.pages.map(function(page, i) {
-				return <Slide key={key++} page={page} previousSlide={me.props.previousSlide} nextSlide={me.props.nextSlide} style={me.props.style} />
-			});
-
 			var questionsNodes = me.props.questions.map(function(question, i) {
-				return <Slide key={key++} question={question} previousSlide={me.props.previousSlide} nextSlide={me.props.nextSlide} style={me.props.style} />
+				return (
+					<Slide
+						key={key++}
+						question={question}
+						previousSlide={me.props.previousSlide}
+						nextSlide={me.props.nextSlide}
+						onAdd={me.props.onAdd}
+						onSubtract={me.props.onSubtract}
+						style={me.props.style}>
+					</Slide>
+				);
 			});
 
 			return (
 				<div className="Slides" style={style}>
-					{pagesNodes}
 					{questionsNodes}
 				</div>
 			);
@@ -96,31 +121,77 @@
 	var Slide = React.createClass({
 
 		render: function() {
-			var key = this.props.key,
+			var me = this,
+				key = me.props.key,
 				buttons = [];
 
 			if (key > 0) {
 				buttons.push(
-					<div key="previous" className="previous" onClick={this.props.previousSlide.bind(null, this)} onTouchEnd={this.props.previousSlide.bind(null, this)}>previous</div>
+					<div key="previous" className="previous" onClick={me.props.previousSlide.bind(null, me)} onTouchEnd={me.props.previousSlide.bind(null, me)}>previous</div>
 				);
 			}
 
-			if (key < (this._owner.props.pages.length) + (this._owner.props.questions.length) - 1) {
+			if (key < (me._owner.props.questions.length - 1)) {
 				buttons.push(
-					<div key="next" className="next" onClick={this.props.nextSlide.bind(null, this)} onTouchEnd={this.props.nextSlide.bind(null, this)}>next</div>
+					<div key="next" className="next" onClick={me.props.nextSlide.bind(null, me)} onTouchEnd={me.props.nextSlide.bind(null, me)}>next</div>
 				);
 			}
 
 			return (
-				<div className="Slide" style={this.props.style}>
-					{this.props.question}{this.props.page}
-					<Draglet />
+				<div className="Slide" style={me.props.style}>
+					{me.props.question.question} - {me.props.question.answer}
+					<AnswerInput question={me.props.question} onAdd={me.props.onAdd.bind(null, me)} onSubtract={me.props.onSubtract.bind(null, me)} />
 					{buttons}
 				</div>
 			);
 		}
 	});
 
+	var AnswerInput = React.createClass({
+		render: function() {
+			return (
+				<div className="AnswerInput">
+					<a href="#" onClick={this.props.onSubtract}>-</a>
+					<input value={this.props.question.answer} readOnly="true" />
+					<a href="#" onClick={this.props.onAdd}>+</a>
+				</div>
+			);
+		}
+	});
+
+	$(document).ready(function() {
+
+		React.initializeTouchEvents(true);
+
+		React.renderComponent(
+			<App />,
+			$('#viewport')[0]
+		);
+		
+	});
+
+})(jQuery, this, document);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/*
 	var Draglet = React.createClass({
 
 		getInitialState: function() {
@@ -140,32 +211,40 @@
 
 			return (
 				<div className="Draglet">
-					<div className="handle" style={{left: this.state.pos.x}} onMouseDown={this.dragStart} onMouseUp={this.dragEnd} onMouseMove={dragHandler}></div>
+					<div
+						className="handle"
+						style={{left: this.state.pos.x}}
+						onMouseDown={this.dragStart}
+						onMouseUp={this.dragEnd}
+						onMouseMove={dragHandler}>
+					</div>
 				</div>
 			);
 		},
 
 		dragStart: function(e) {
-			if (e.button !== 0) return; // only left mouse button
+			//if (e.button !== 0) return; // only left mouse button
 			var pos = $(this.getDOMNode()).offset();
 			this.setState({
 				dragging: true,
 				rel: {
-					x: e.pageX - pos.left
+					x: e.pageX - pos.left // @todo calculate based on Draglet instead of Page
 				}
 			});
+
 			e.stopPropagation();
 			e.preventDefault();
 		},
 
 		dragEnd: function(e) {			
 			this.setState({dragging: false});
+
 			e.stopPropagation();
 			e.preventDefault();
 		},
 
 		dragHandler: function(e) {
-			//console.log($.extend(true, {}, e));
+			//console.log($.extend({}, e));
 
 			if (!this.state.dragging) return;
 
@@ -174,21 +253,10 @@
 					x: e.pageX - this.state.rel.x
 				}
 			});
+
 			e.stopPropagation();
 			e.preventDefault();
 		},
 
 	});
-
-	$(document).ready(function() {
-
-		React.initializeTouchEvents(true);
-
-		React.renderComponent(
-			<App />,
-			document.getElementById('viewport')
-		);
-		
-	});
-
-})(jQuery, this, document);
+	*/
