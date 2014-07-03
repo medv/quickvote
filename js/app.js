2f52855fa93d84f2a02f42e9a01bbe3af0ee990c
medv <mikhail.advent@gmail.com>
2014-07-04 01:33:57 +0700

    added results slide and some experimental answer logic and text

--- js/app.js
+++ js/app.js
@@ -10,9 +10,6 @@
 		getInitialState: function() {
 			return {
 				slides: [
-					{type: 'content', title: 'About', content: 'Some About content'},
-					{type: 'content', title: 'Privacy', content: 'Some Privacy content'},
-
 					{type: 'question', title: 'Nuclear power?', answer: 0},			
 					{type: 'question', title: 'Auckland roads?', answer: 0},
 					{type: 'question', title: 'Offshore drilling?', answer: 0},
@@ -37,7 +34,7 @@
 
 		render: function() {
 			var me = this;
-			
+
 			return (
 				<div className="App" style={me.state.style}>
 					<Slides
@@ -58,18 +55,20 @@
 		onSubtract: function(slide) {
 			var newState = $.extend({}, this.state),
 				questionKey = slide.props.key,
-				answer = newState.questions[questionKey].answer - 1;
+				newAnswer = newState.slides[questionKey].answer - 1,
+				answer = newAnswer < -2 ? -2 : newAnswer; // cap at -2
 
-			newState.questions[questionKey].answer = answer;
+			newState.slides[questionKey].answer = answer;
 			this.setState(newState, this.onAnswer(questionKey, answer));
 		},
 
 		onAdd: function(slide) {
 			var newState = $.extend({}, this.state),
 				questionKey = slide.props.key,
-				answer = newState.questions[questionKey].answer + 1;
+				newAnswer = newState.slides[questionKey].answer + 1,
+				answer = newAnswer > 2 ? 2 : newAnswer; // cap at +2
 
-			newState.questions[questionKey].answer = answer;
+			newState.slides[questionKey].answer = answer;
 			this.setState(newState, this.onAnswer(questionKey, answer));					
 		},
 
@@ -120,42 +119,33 @@
 
 		render: function() {
 			var me = this,
-				key = 0,
 				style,
-				questionNodes,
-				resultNode;
+				slideNodes;
 
 			style = {
-				left: -(me.props.currentSlide * me.props.style.width),
-				width: me.props.style.width * me.props.questions.length,
+				left: -(me.props.currentSlide * me.props.style.width), // offset Slides to show current Slide
+				width: me.props.style.width * me.props.slides.length,
 				height: me.props.style.height
 			}
 
-			questionsNodes = me.props.questions.map(function(question, i) {
+			slideNodes = me.props.slides.map(function(slide, i) {
 				return (
 					<Slide
-						key={key++}
-						question={question}
+						key={i}
+						slide={slide}
+						style={me.props.style}
+
 						previousSlide={me.props.previousSlide}
 						nextSlide={me.props.nextSlide}
 						onAdd={me.props.onAdd}
 						onSubtract={me.props.onSubtract}
-						style={me.props.style}>
-					</Slide>
+					></Slide>
 				);
 			});
 
-			resultNode = (
-				<Slide
-					key={key}
-					previousSlide={me.props.previousSlide}
-					style={me.props.style}>
-				</Slide>
-			);
-
 			return (
 				<div className="Slides" style={style}>
-					{questionsNodes}
+					{slideNodes}
 				</div>
 			);
 		}
@@ -187,7 +177,7 @@
 				);
 			}
 
-			if (key < (me._owner.props.questions.length - 1)) {
+			if (key < (me._owner.props.slides.length - 1)) {
 				buttons.push(
 					<div key="next" className="next" onClick={me.props.nextSlide.bind(null, me)} onTouchEnd={me.props.nextSlide.bind(null, me)}>next</div>
 				);
@@ -198,15 +188,75 @@
 
 		render: function() {
 			var me = this,
+				slide = me.props.slide,
+				contentNode,
+				answerInputNode,
 				buttons;
+
+			if (typeof slide.answer !== 'undefined') {
+				var answer = slide.answer,
+					answerString;
+
+				answerString = 'You are indifferent';
+				if (answer == -2) {
+					answerString = 'You strongly disagree';
+				}
+				else if (answer == -1) {
+					answerString = 'You lean towards disagreeing';
+				}
+				else if (answer == 2) {
+					answerString = 'You strongly agree';
+				}
+				else if (answer == 1) {
+					answerString = 'You lean towards agreeing';
+				}
+
+				contentNode = <p>{answerString}</p>;
+				answerInputNode = <AnswerInput question={slide} onAdd={me.props.onAdd.bind(null, me)} onSubtract={me.props.onSubtract.bind(null, me)} />;
+			}
+			else {
+				var score,
+					resultsNodes,
+					verdict;
+
+				score = 0;
+				resultsNodes = me._owner.props.slides.map(function(slide, i) {
+						if (slide.type === 'question') {
+							score = score + slide.answer;
+							return <li key={i}>{slide.title} <span className="results-answer">{slide.answer}</span></li>
+						}
+					});
+
+				verdict = 'Congratulations. You don\'t give a shit';
+				if (score < -4) {
+					verdict = 'Some trees need huggin\'';
+				}
+				else if (score < 0) {
+					verdict = 'Down with the man.';
+				}
+				else if (score > 4) {
+					verdict = 'Yeah well... that\'s like, your opinion, man.';
+				}
+				else if (score > 0) {
+					verdict = 'Cool story bro.';
+				}
+
+				contentNode = (
+					<div>
+						{slide.content}
+						<ul>{resultsNodes}</ul>
+						<p>You scored {score}. {verdict}</p>
+					</div>
+				);
+			}
 
 			buttons = me.getButtons();
 
 			return (
 				<div className="Slide" style={me.props.style}>
-					<p>Q{me.props.key + 1}. {me.props.question.question}</p>
-					<p>Your answer: {me.props.question.answer}</p>
-					<AnswerInput question={me.props.question} onAdd={me.props.onAdd.bind(null, me)} onSubtract={me.props.onSubtract.bind(null, me)} />
+					<h2>{me.props.slide.title}</h2>
+					{contentNode}
+					{answerInputNode}
 					{buttons}
 				</div>
 			);
@@ -228,11 +278,19 @@
 	 */
 	var AnswerInput = React.createClass({
 		render: function() {
+			var answer,
+				subtractVisibility,
+				addVisibility;
+
+			answer = this.props.question.answer;
+			subtractVisibility = answer > -2 ? 'visible' : 'hidden';
+			addVisibility = answer < 2 ? 'visible' : 'hidden';
+
 			return (
 				<div className="AnswerInput">
-					<a href="#" onClick={this.props.onSubtract}>-</a>
+					<a href="#" style={{visibility: subtractVisibility}} onClick={this.props.onSubtract}>-</a>
 					<input value={this.props.question.answer} readOnly="true" />
-					<a href="#" onClick={this.props.onAdd}>+</a>
+					<a href="#" style={{visibility: addVisibility}} onClick={this.props.onAdd}>+</a>
 				</div>
 			);
 		}
